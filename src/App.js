eb7aa90c34350571023848c3aec1d53f37fca31a
Julien Dubois <dubois.julien.mail@gmail.com>
2022-01-23 10:27:38 +0100

    [PROJECT] create react app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Settings from './pages/Settings'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
+import './App.css'
+
+function App() {
+    return (
+        <div className='App'>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/settings' element={<Settings />} />
+                <Route path='/signin' element={<SignIn />} />
+                <Route path='/signup' element={<SignUp />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default App;
